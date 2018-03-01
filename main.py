2dcad9b8c0bee63a89a269728c0813047b158f0e
JellyWX <judewrs@gmail.com>
2018-03-01 21:43:26 +0000

    csgo and insurgency stuff kind of

--- main.py
+++ main.py
@@ -7,160 +7,194 @@ import json
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot
+import aiohttp
+
 
 class TrackerClient(discord.Client):
-  def __init__(self, *args, **kwargs):
-    super(TrackerClient, self).__init__(*args, **kwargs)
-    self.tracker = Tracker(self)
-    self.no_track = []
-    self.commands = {
-      'ping' : self.ping,
-      'stats' : self.stats,
-      'track' : self.track,
-      'help' : self.help,
-      'chart' : self.chart
-    }
+    def __init__(self, *args, **kwargs):
+        super(TrackerClient, self).__init__(*args, **kwargs)
+        self.tracker = Tracker(self)
+        self.no_track = []
+        self.commands = {
+            'ping' : self.ping,
+            'stats' : self.stats,
+            'track' : self.track,
+            'help' : self.help,
+            'chart' : self.chart,
+            'insurgency' : self.insurgency,
+            'csgo' : self.csgo
+        }
 
-    try:
-      with open('USER_TRACKED.json', 'r') as f:
-        self.no_track = json.load(f)
-    except FileNotFoundError:
-      with open('USER_TRACKED.json', 'w') as f:
-        json.dump([], f)
-
-  async def on_ready(self):
-    print('Online now!')
-    await client.change_presence(game=discord.Game(name='@TrackerBot help'))
-
-  async def on_message(self, message):
-    if not await self.get_cmd(message):
-      pass
-
-  async def get_cmd(self, message):
-    if self.user.id in map(lambda x: x.id, message.mentions) and len(message.content.split(' ')) > 1:
-      if message.content.split(' ')[1] in self.commands.keys():
-        await self.commands[message.content.split(' ')[1]](message)
-        return True
-
-      return False
-
-    elif isinstance(message.channel, discord.DMChannel):
-      if message.content.split(' ')[0] in self.commands.keys():
-        await self.commands[message.content.split(' ')[0]](message)
-        return True
-
-      return False
-
-  async def ping(self, message):
-    t = message.created_at.timestamp()
-    e = await message.channel.send('pong')
-    delta = e.created_at.timestamp() - t
-    await e.edit(content='Pong! {}ms round trip'.format(round(delta * 1000)))
-
-  async def stats(self, message):
-    target = [x for x in message.content.split(' ')[1:] if x.startswith('<@')]
-
-    if len(target) > 0:
-      target = target[0][1:-1]
-      while target[0] not in '0123456789':
         try:
-          target = target[1:]
-        except IndexError:
-          await message.channel.send('Error deciphering user tagged. Make sure it\'s formatted correctly!')
-          return
+            with open('USER_TRACKED.json', 'r') as f:
+                self.no_track = json.load(f)
+        except FileNotFoundError:
+            with open('USER_TRACKED.json', 'w') as f:
+                json.dump([], f)
 
-      target = self.get_user(int(target))
-      if target == None:
-        await message.channel.send('Couldn\'t find user tagged. Are you sure they\'re real?')
-        return
+    async def on_ready(self):
+        print('Online now!')
+        await client.change_presence(game=discord.Game(name='@TrackerBot help'))
 
-    else:
-      target = message.author
+    async def on_message(self, message):
+        if not await self.get_cmd(message):
+            pass
 
-    em = discord.Embed(title='{}\'s stats'.format(target.name))
-    for key, data in self.tracker.getUser(target.id).items():
-      em.add_field(name=key, value='{} minutes'.format(round((data * self.tracker.INTERVAL)/60)))
-    await message.channel.send(embed=em)
+    async def get_cmd(self, message):
+        if self.user.id in map(lambda x: x.id, message.mentions) and len(message.content.split(' ')) > 1:
+            if message.content.split(' ')[1] in self.commands.keys():
+                await self.commands[message.content.split(' ')[1]](message)
+                return True
 
-  async def track(self, message):
-    if 'disable' in message.content:
-      await message.channel.send('Disabled tracking :thumbsup:')
-      self.no_track.append(message.author.id)
-    elif message.author.id in self.no_track:
-      await message.channel.send('Enabled tracking :thumbsup:')
-      self.no_track.remove(message.author.id)
-    else:
-      await message.channel.send('Tracking is currently enabled for you. Use `track disable` to disable tracking')
+            return False
 
-    with open('USER_TRACKED.json', 'w') as f:
-      json.dump(self.no_track, f)
+        elif isinstance(message.channel, discord.DMChannel):
+            if message.content.split(' ')[0] in self.commands.keys():
+                await self.commands[message.content.split(' ')[0]](message)
+                return True
 
-  async def help(self, message):
-    await message.channel.send(embed=discord.Embed(
-      description='''
+            return False
+
+    async def ping(self, message):
+        t = message.created_at.timestamp()
+        e = await message.channel.send('pong')
+        delta = e.created_at.timestamp() - t
+        await e.edit(content='Pong! {}ms round trip'.format(round(delta * 1000)))
+
+    async def stats(self, message):
+        target = [x for x in message.content.split(' ')[1:] if x.startswith('<@')]
+
+        if len(target) > 0:
+            target = target[0][1:-1]
+            while target[0] not in '0123456789':
+                try:
+                    target = target[1:]
+                except IndexError:
+                    await message.channel.send('Error deciphering user tagged. Make sure it\'s formatted correctly!')
+                    return
+
+            target = self.get_user(int(target))
+            if target == None:
+                await message.channel.send('Couldn\'t find user tagged. Are you sure they\'re real?')
+                return
+
+        else:
+            target = message.author
+
+        em = discord.Embed(title='{}\'s stats'.format(target.name))
+        for key, data in self.tracker.getUser(target.id).items():
+            em.add_field(name=key, value='{} minutes'.format(round((data * self.tracker.INTERVAL)/60)))
+        await message.channel.send(embed=em)
+
+    async def track(self, message):
+        if 'disable' in message.content:
+            await message.channel.send('Disabled tracking :thumbsup:')
+            self.no_track.append(message.author.id)
+        elif message.author.id in self.no_track:
+            await message.channel.send('Enabled tracking :thumbsup:')
+            self.no_track.remove(message.author.id)
+        else:
+            await message.channel.send('Tracking is currently enabled for you. Use `track disable` to disable tracking')
+
+        with open('USER_TRACKED.json', 'w') as f:
+            json.dump(self.no_track, f)
+
+    async def help(self, message):
+        await message.channel.send(embed=discord.Embed(
+            description='''
 `help` : Show this page
 `stats [mention]` : Get online stats for a user
 `track [disable]` : Enable or disable tracking for yourself
 `chart [ignore="Offline,Online,GameName..."]` : Generate a pie chart of your activities
-      '''
-    ))
+            '''
+        ))
 
-  async def chart(self, message):
-    user = self.tracker.getUser(message.author.id)
+    async def chart(self, message):
+        user = self.tracker.getUser(message.author.id)
 
-    ignore_list = []
-    listing = False
-    for i in message.content.split(' '):
-      if i.startswith('ignore="'):
-        ignore_list.append(i)
-        listing = True
-      elif listing:
-        ignore_list.append(i)
-      elif i[-1] == '"':
-        ignore_list.append(i)
+        ignore_list = []
         listing = False
-        break
+        for i in message.content.split(' '):
+            if i.startswith('ignore="'):
+                ignore_list.append(i)
+                listing = True
+            elif listing:
+                ignore_list.append(i)
+            elif i[-1] == '"':
+                ignore_list.append(i)
+                listing = False
+                break
 
-    if len(ignore_list) > 0:
-      ignore_list = ' '.join(ignore_list)
-      listed = ignore_list.split('"')[1].replace('"', '').split(',')
-      listed = [i.strip().lower() for i in listed]
-      print(listed)
-      user = {k: v for k, v in user.items() if k.lower() not in listed}
+        if len(ignore_list) > 0:
+            ignore_list = ' '.join(ignore_list)
+            listed = ignore_list.split('"')[1].replace('"', '').split(',')
+            listed = [i.strip().lower() for i in listed]
+            print(listed)
+            user = {k: v for k, v in user.items() if k.lower() not in listed}
 
-    pyplot.clf()
-    pyplot.axis('equal')
-    t = sum(user.values())
-    pyplot.pie(user.values(), labels=user.keys(), autopct=lambda x: '{}mins'.format(int((((x * t) / 100) * self.tracker.INTERVAL) / 60)))
-    pyplot.savefig('curr.png')
+        pyplot.clf()
+        pyplot.axis('equal')
+        t = sum(user.values())
+        pyplot.pie(user.values(), labels=user.keys(), autopct=lambda x: '{}mins'.format(int((((x * t) / 100) * self.tracker.INTERVAL) / 60)))
+        pyplot.savefig('curr.png')
 
-    f = open('curr.png', 'rb')
-    await message.channel.send(file=discord.File(f, 'chart.png'))
-    f.close()
+        f = open('curr.png', 'rb')
+        await message.channel.send(file=discord.File(f, 'chart.png'))
+        f.close()
 
-  async def Update(self):
-    await client.wait_until_ready()
-    while not client.is_closed():
-      await self.tracker.Update()
+    async def Update(self):
+        await client.wait_until_ready()
+        while not client.is_closed():
+            await self.tracker.Update()
 
-      print(self.tracker.data)
-      await asyncio.sleep(self.tracker.INTERVAL)
+            await asyncio.sleep(self.tracker.INTERVAL)
 
-  def get_patrons(self, level='Patrons'):
-    p_server = self.get_guild(350391364896161793)
-    p_role = discord.utils.get(p_server.roles, name=level)
-    premiums = [user for user in p_server.members if p_role in user.roles]
+    def get_patrons(self, level='Patrons'):
+        p_server = self.get_guild(350391364896161793)
+        p_role = discord.utils.get(p_server.roles, name=level)
+        premiums = [user for user in p_server.members if p_role in user.roles]
 
-    return premiums
+        return premiums
+
+    async def insurgency(self, message):
+        await self.grab_steam_game(222880, message)
+
+    async def csgo(self, message):
+        await self.grab_steam_game(730, message)
+
+    async def grab_steam_game(self, game, message):
+        if len(message.content.split(' ')) != 3:
+            await message.channel.send('Please enter your SteamID 64 with the command. You can find yours at https://steamidfinder.com/')
+            return
+
+        player_id = message.content.split(' ')[-1]
+
+        r = await self.fetch_url('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={}&key={}&steamid={}'.format(game, steam_key, player_id))
+        print(r)
+        if r:
+            await message.channel.send(r['playerstats']['stats'])
+        else:
+            await message.channel.send('No data could be found. Are you sure that user exists?')
+
+    async def fetch_url(self, url):
+        print(url)
+
+        async with aiohttp.ClientSession() as sesh:
+            async with sesh.get(url) as r:
+                print(r.status)
+                return await r.json()
 
 
 try: ## token grabbing code
-  with open('token','r') as token_f:
-    token = token_f.read().strip('\n')
+    with open('token', 'r') as token_f:
+        token = token_f.read().strip('\n')
+
+    with open('steam_key', 'r') as token_f:
+        steam_key = token_f.read().strip('\n')
 
 except:
-  print('no token provided')
-  sys.exit(-1)
+    print('no token/s provided')
+    sys.exit(-1)
 
 client = TrackerClient()
 client.loop.create_task(client.Update())
--- tracker.py
+++ tracker.py
@@ -4,75 +4,76 @@ import discord
 
 class Tracker(object):
 
-  def __init__(self, client):
-    self.client = client
-    self.data = {'self' : {'Uptime' : 0}}
-    self.INTERVAL = 20
+    def __init__(self, client):
+        self.client = client
+        self.data = {'self' : {'Uptime' : 0}}
+        self.INTERVAL = 20
 
-    try:
-      with open('USER_DATA', 'rb') as f:
-        self.data = msgpack.unpack(f, encoding='utf8')
-    except FileNotFoundError:
-      with open('USER_DATA', 'wb') as f:
-        msgpack.pack(self.data, f)
+        try:
+            with open('USER_DATA', 'rb') as f:
+                self.data = msgpack.unpack(f, encoding='utf8')
+        except FileNotFoundError:
+            with open('USER_DATA', 'wb') as f:
+                msgpack.pack(self.data, f)
 
-  async def Update(self):
-    with open('USER_DATA-backup', 'wb') as f:
-      msgpack.pack(self.data, f)
+    async def Update(self):
+        with open('USER_DATA-backup', 'wb') as f:
+            msgpack.pack(self.data, f)
 
-    self.clear()
+        self.clear()
 
-    self.data['self']['Uptime'] += 1
+        self.data['self']['Uptime'] += 1
 
-    members = []
-    member_ids = []
-    patrons = self.client.get_patrons('Donor')
-    for member in self.client.get_all_members():
-      if member.id in member_ids:
-        continue
-      else:
-        members.append(member)
-        member_ids.append(member.id)
+        members = []
+        member_ids = []
+        patrons = self.client.get_patrons('Donor')
+        for member in self.client.get_all_members():
+            if member.id in member_ids:
+                continue
+            else:
+                members.append(member)
+                member_ids.append(member.id)
 
-    for member in members:
+        for member in members:
 
-      if member.bot or (member not in patrons and member not in self.client.get_guild(398546681618104323).members):
-        continue
+            if member.bot or (member not in patrons and member not in self.client.get_guild(398546681618104323).members):
+                continue
 
-      if member.id in self.client.no_track:
-        self.data[member.id] = {'Online' : 0, 'Idle' : 0, 'DnD' : 0}
-        continue
+            if member.id in self.client.no_track:
+                self.data[member.id] = {'Online' : 0, 'Idle' : 0, 'DnD' : 0}
+                continue
 
-      if member.id not in self.data.keys():
-        self.data[member.id] = {'Online' : 0, 'Idle' : 0, 'DnD' : 0}
+            if member.id not in self.data.keys():
+                self.data[member.id] = {'Online' : 0, 'Idle' : 0, 'DnD' : 0}
 
-      if member.game == None:
-        if member.status == discord.Status.online:
-          self.data[member.id]['Online'] += 1
-        elif member.status == discord.Status.idle:
-          self.data[member.id]['Idle'] += 1
-        elif member.status == discord.Status.dnd:
-          self.data[member.id]['DnD'] += 1
-        continue
+            if member.game == None:
+                if member.status == discord.Status.online:
+                    self.data[member.id]['Online'] += 1
+                elif member.status == discord.Status.idle:
+                    self.data[member.id]['Idle'] += 1
+                elif member.status == discord.Status.dnd:
+                    self.data[member.id]['DnD'] += 1
+                continue
 
-      if member.game.name not in self.data[member.id].keys():
-        self.data[member.id][member.game.name] = 0
+            if member.game.name not in self.data[member.id].keys():
+                self.data[member.id][member.game.name] = 0
 
-      self.data[member.id][member.game.name] += 1
+            self.data[member.id][member.game.name] += 1
 
-    with open('USER_DATA', 'wb') as f:
-      msgpack.pack(self.data, f)
+        with open('USER_DATA', 'wb') as f:
+            msgpack.pack(self.data, f)
 
-  def getUser(self, id : int):
-    if id not in self.data.keys():
-      return False
-    else:
-      e = {x : y for x, y in self.data[id].items() if y > 0}
-      offline = self.data['self']['Uptime'] - sum(e.values())
-      if offline > 0:
-        e['Offline'] = offline
-      return e
+    def getUser(self, id : int):
+        if id not in self.data.keys():
+            return False
+        else:
+            e = {x : y for x, y in self.data[id].items() if y > 0}
+            offline = self.data['self']['Uptime'] - sum(e.values())
+            if offline > 0:
+                e['Offline'] = offline
+            return e
 
-  def clear(self):
-    if datetime.datetime.now().strftime('%A-%H-%M') == 'Monday-20-00':
-      self.data = {'self' : {'Uptime' : 0}}
+    def clear(self):
+        if datetime.datetime.now().strftime('%A-%H-%M') == 'Monday-20-00':
+            self.data = {'self' : {'Uptime' : 0}}
+
